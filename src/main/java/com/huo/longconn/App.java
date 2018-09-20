6e03e84ce9412aed0272481c3d05a76c468e20d2
xiao5aha <846137167>
2018-09-20 20:49:13 +0800

    增加log4j2日志配置

--- src/main/java/com/huo/longconn/App.java
+++ src/main/java/com/huo/longconn/App.java
@@ -1,10 +1,19 @@
 package com.huo.longconn;
 
+import lombok.extern.log4j.Log4j2;
+
 /**
  * Hello world!
  */
+@Log4j2
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (; ; ) {
+
+            log.debug("debug");
+            log.info("info");
+            log.error("error");
+        }
     }
 }
